8edc572a0b4113d9ba9135b5e39bc2665327e4c7
Muzaffar Mohammed <50576677+MuzaffarMohammed@users.noreply.github.com>
2020-09-10 21:03:30 +0530

    Added simple login and basic home page

--- src/main/java/com/mvc/spring/controllers/LoginController.java
+++ src/main/java/com/mvc/spring/controllers/LoginController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,11 +13,36 @@ import com.mvc.spring.model.LoginUser;
 @Controller
 public class LoginController {
 	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String loginPage(Model model, HttpSession session) {
+		session.setAttribute("title", "MHP");
+		model.addAttribute("loginUser", new LoginUser());
+		return "index";
+	}
+	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login(Model model, HttpSession session) {
 		session.setAttribute("title", "MHP");
-		model.addAttribute("loginUser", new LoginUser("Muzaffar", "test123"));
-		return "login";
+		model.addAttribute("loginUser", new LoginUser());
+		return "index";
+	}
+	
+	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+	public String authenticate(@ModelAttribute LoginUser loginUser, HttpSession session) {
+		session.setAttribute("title", "MHP");
+		
+		if(loginUser.getUserName().equals("Test")) {
+			return "redirect:/home";
+		}else {
+			return "index";
+		}
+		
+	}
+	
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String login() {
+		
+		return "home";
 	}
 
 }
